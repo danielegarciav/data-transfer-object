643ec16b9bfbdff6cbfa4e1564cf3557ef59d82e
Daniel Garcia <daniel.garcia@mail.utoronto.ca>
2021-01-27 12:33:59 -0500

    chore(eslint): allow unused vars starting with _

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
   ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
   env: {
     es2020: true,
     node: true,
